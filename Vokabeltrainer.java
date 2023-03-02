3ae870fcca4e30c942d2467757c8f1ea82aded60
Johannes Häusler <mail@spotlightforbugs.eu>
2023-03-02 09:17:37 +0100

    Changed Vokabel.Java and Vokabeltrainer.java

--- Vokabeltrainer.java
+++ Vokabeltrainer.java
@@ -1,8 +1,8 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,163 +11,76 @@ public class Vokabeltrainer {
     private List<Vokabel> bekannteVokabeln;
     private List<Vokabel> falscheVokabeln;
     private List<Vokabel> neueVokabeln;
-    private boolean deNachEn;
+    private File datei;
 
-    public Vokabeltrainer(String dateiname) {
+    public Vokabeltrainer() {
         alleVokabeln = new ArrayList<Vokabel>();
         bekannteVokabeln = new ArrayList<Vokabel>();
         falscheVokabeln = new ArrayList<Vokabel>();
         neueVokabeln = new ArrayList<Vokabel>();
-        deNachEn = true;
-        try (BufferedReader br = new BufferedReader(new FileReader(dateiname))) {
-            String line;
-            while ((line = br.readLine()) != null) {
+    }
+
+    public void ladeVokabeln(String dateiname) {
+        alleVokabeln.clear();
+        bekannteVokabeln.clear();
+        falscheVokabeln.clear();
+        neueVokabeln.clear();
+        datei = new File(dateiname);
+        try {
+            Scanner scanner = new Scanner(datei);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
                 String[] parts = line.split(";");
                 if (parts.length == 2) {
-                    Vokabel v = new Vokabel(parts[0].trim(), parts[1].trim());
+                    Vokabel v = new Vokabel(parts[0], parts[1]);
                     alleVokabeln.add(v);
                     neueVokabeln.add(v);
                 }
             }
-        } catch (IOException e) {
-            System.err.println("Fehler beim Lesen der Datei: " + e.getMessage());
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Fehler beim Lesen der Datei " + dateiname + ": " + e.getMessage());
         }
     }
 
-    public void setDeNachEn(boolean deNachEn) {
-        this.deNachEn = deNachEn;
+    public List<Vokabel> getAlleVokabeln() {
+        return alleVokabeln;
     }
 
-    public void start() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Bitte wählen Sie einen Modus:");
-            System.out.println("1 - Alle Vokabeln abfragen");
-            System.out.println("2 - Bekannte Vokabeln abfragen");
-            System.out.println("3 - Falsche Vokabeln abfragen");
-            System.out.println("4 - Neue Vokabeln abfragen");
-            System.out.println("5 - Richtung umkehren");
-            System.out.println("0 - Beenden");
-            int modus = scanner.nextInt();
-            if (modus == 0) {
-                break;
-            }
-            List<Vokabel> vokabeln;
-            switch (modus) {
-                case 1:
-                    vokabeln = alleVokabeln;
-                    break;
-                case 2:
-                    vokabeln = bekannteVokabeln;
-                    break;
-                case 3:
-                    vokabeln = falscheVokabeln;
-                    break;
-                case 4:
-                    vokabeln = neueVokabeln;
-                    break;
-                default:
-                    System.out.println("Ungültiger Modus.");
-                    continue;
-            }
-            if (vokabeln.isEmpty()) {
-                System.out.println("Keine Vokabeln vorhanden.");
-                continue;
-            }
-            System.out.println("Bitte geben Sie 'start' ein, um die Abfrage zu beginnen.");
-            scanner.nextLine();
-            String input = scanner.nextLine().trim();
-            if (!input.equalsIgnoreCase("start")) {
-                System.out.println("Ungültige Eingabe.");
-                continue;
-            }
-            Collections.shuffle(vokabeln);
-            int numCorrect = 0;
-            for (Vokabel v : vokabeln) {
-                System.out.println("Was ist die Übersetzung von "" + (deNachEn ? v.getDeutsch() : v.getEnglisch()) + ""?");
-                String antwort = scanner.nextLine().trim();
-                boolean korrekt = (deNachEn ? v.getEnglisch() : v.getDeutsch()).equalsIgnoreCase(antwort);
-                if (korrekt) {
-                    System.out.println("Richtig!");
-                    numCorrect++;
-                    if (modus == 4) {
-                        neueVokabeln.remove(v);
-                        bekannteVokabeln.add(v);
-                        v.setBekannt(true);
-                    }
-                } else {
-                    System.out.println("Falsch! Die korrekte Antwort wäre "" + (deNachEn ? v.getEnglisch() : v.getDeutsch()) + "" gewesen.");
-                    if (modus == 4) {
-                        neueVokabeln.remove(v);
-                        falscheVokabeln.add(v);
-                        v.setFalsch(true);
-                    }
-                }
-            }
-            System.out.println("Abfrage beendet. Sie haben " + numCorrect + " von " + vokabeln.size() + " Vokabeln korrekt beantwortet.");
-        }
-        scanner.close();
+    public List<Vokabel> getBekannteVokabeln() {
+        return bekannteVokabeln;
     }
 
-    System.out.println("5 - Richtung umkehren");
-System.out.println("0 - Beenden");
-    int modus = scanner.nextInt();
-if (modus == 0) {
-        break;
+    public List<Vokabel> getFalscheVokabeln() {
+        return falscheVokabeln;
     }
-    List<Vokabel> vokabeln;
-switch (modus) {
-        case 1:
-            vokabeln = alleVokabeln;
-            break;
-        case 2:
-            vokabeln = bekannteVokabeln;
-            break;
-        case 3:
-            vokabeln = falscheVokabeln;
-            break;
-        case 4:
-            vokabeln = neueVokabeln;
-            break;
-        default:
-            System.out.println("Ungültiger Modus.");
-            continue;
+
+    public List<Vokabel> getNeueVokabeln() {
+        return neueVokabeln;
     }
-if (vokabeln.isEmpty()) {
-        System.out.println("Keine Vokabeln vorhanden.");
-        continue;
-    }
-System.out.println("Bitte geben Sie 'start' ein, um die Abfrage zu beginnen.");
-scanner.nextLine();
-    String input = scanner.nextLine().trim();
-if (!input.equalsIgnoreCase("start")) {
-        System.out.println("Ungültige Eingabe.");
-        continue;
-    }
-Collections.shuffle(vokabeln);
-    int numCorrect = 0;
-for (Vokabel v : vokabeln) {
-        System.out.println("Was ist die Übersetzung von "" + (deNachEn ? v.getDeutsch() : v.getEnglisch()) + ""?");
-        String antwort = scanner.nextLine().trim();
-        boolean korrekt = (deNachEn ? v.getEnglisch() : v.getDeutsch()).equalsIgnoreCase(antwort);
-        if (korrekt) {
-            System.out.println("Richtig!");
-            numCorrect++;
-            if (modus == 4) {
-                neueVokabeln.remove(v);
-                bekannteVokabeln.add(v);
-                v.setBekannt(true);
+
+    public void beantworteVokabel(Vokabel vokabel, boolean richtigBeantwortet) {
+        if (richtigBeantwortet) {
+            neueVokabeln.remove(vokabel);
+            if (!bekannteVokabeln.contains(vokabel)) {
+                bekannteVokabeln.add(vokabel);
             }
         } else {
-            System.out.println("Falsch! Die korrekte Antwort wäre "" + (deNachEn ? v.getEnglisch() : v.getDeutsch()) + "" gewesen.");
-            if (modus == 4) {
-                neueVokabeln.remove(v);
-                falscheVokabeln.add(v);
-                v.setFalsch(true);
+            if (!falscheVokabeln.contains(vokabel)) {
+                falscheVokabeln.add(vokabel);
             }
         }
     }
-System.out.println("Abfrage beendet. Sie haben " + numCorrect + " von " + vokabeln.size() + " Vokabeln korrekt beantwortet.");
-}
-scanner.close();
+
+    public void speichereVokabeln() {
+        try {
+            FileWriter writer = new FileWriter(datei);
+            for (Vokabel v : alleVokabeln) {
+                writer.write(v.getDeutsch() + ";" + v.getEnglisch() + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Fehler beim Schreiben der Datei " + datei.getName() + ": " + e.getMessage());
         }
+    }
+}
---
+++ VokabeltrainerGUI.java
@@ -0,0 +1,146 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class VokabeltrainerGUI extends JFrame {
+
+    private Vokabeltrainer trainer;
+    private JTextArea ausgabe;
+    private JComboBox<String> modusAuswahl;
+    private JButton abfrageButton, bearbeitenButton;
+    private JRadioButton deNachEnRadio, enNachDeRadio;
+
+    public VokabeltrainerGUI(Vokabeltrainer trainer) {
+        super("Vokabeltrainer");
+        this.trainer = trainer;
+
+        // Oberfläche erstellen
+        JPanel obenPanel = new JPanel(new BorderLayout());
+        modusAuswahl = new JComboBox<String>(new String[] {"Alle Vokabeln", "Bekannte Vokabeln", "Falsche Vokabeln", "Neue Vokabeln"});
+        obenPanel.add(modusAuswahl, BorderLayout.CENTER);
+        abfrageButton = new JButton("Abfrage starten");
+        obenPanel.add(abfrageButton, BorderLayout.EAST);
+        add(obenPanel, BorderLayout.NORTH);
+
+        JPanel mittePanel = new JPanel(new BorderLayout());
+        ausgabe = new JTextArea();
+        ausgabe.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(ausgabe);
+        mittePanel.add(scrollPane, BorderLayout.CENTER);
+        JPanel radioPanel = new JPanel(new GridLayout(1,2));
+        deNachEnRadio = new JRadioButton("Deutsch -> Englisch");
+        enNachDeRadio = new JRadioButton("Englisch -> Deutsch");
+        ButtonGroup richtungsgruppe = new ButtonGroup();
+        richtungsgruppe.add(deNachEnRadio);
+        richtungsgruppe.add(enNachDeRadio);
+        deNachEnRadio.setSelected(true);
+        radioPanel.add(deNachEnRadio);
+        radioPanel.add(enNachDeRadio);
+        mittePanel.add(radioPanel, BorderLayout.SOUTH);
+        add(mittePanel, BorderLayout.CENTER);
+
+        bearbeitenButton = new JButton("Vokabeln bearbeiten");
+        add(bearbeitenButton, BorderLayout.SOUTH);
+
+        // Event-Handler hinzufügen
+        abfrageButton.addActionListener(new AbfrageButtonHandler());
+        bearbeitenButton.addActionListener(new BearbeitenButtonHandler());
+
+        // Fenster konfigurieren
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setVisible(true);
+    }
+
+    private class AbfrageButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int modusIndex = modusAuswahl.getSelectedIndex();
+            boolean deNachEn = deNachEnRadio.isSelected();
+            List<Vokabel> vokabeln;
+            switch (modusIndex) {
+                case 0:
+                    vokabeln = trainer.getAlleVokabeln();
+                    break;
+                case 1:
+                    vokabeln = trainer.getBekannteVokabeln();
+                    break;
+                case 2:
+                    vokabeln = trainer.getFalscheVokabeln();
+                    break;
+                case 3:
+                    vokabeln = trainer.getNeueVokabeln();
+                    break;
+                default:
+                    return;
+            }
+            if (vokabeln.isEmpty()) {
+                ausgabe.setText("Keine Vokabeln vorhanden.");
+                return;
+            }
+            ausgabe.setText("");
+            Collections.shuffle(vokabeln);
+            int numCorrect = 0;
+            for (Vokabel v : vokabeln) {
+                String frage = deNachEn ? v.getDeutsch() : v.getEnglisch();
+                String antwort = JOptionPane.showInputDialog(null, frage);
+                if (antwort == null) {
+// Abbruch durch Benutzer
+                    ausgabe.setText("Abbruch durch Benutzer.");
+                    return;
+                }
+                if (antwort.equalsIgnoreCase(deNachEn ? v.getEnglisch() : v.getDeutsch())) {
+                    ausgabe.append("Richtig!\n");
+                    numCorrect++;
+                    trainer.beantworteVokabel(v, true);
+                } else {
+                    ausgabe.append("Falsch! Richtig wäre "" + (deNachEn ? v.getEnglisch() : v.getDeutsch()) + "" gewesen.\n");
+                    trainer.beantworteVokabel(v, false);
+                }
+            }
+            ausgabe.append("\nErgebnis: " + numCorrect + "/" + vokabeln.size() + " richtig beantwortet.");
+        }
+    }
+
+
+    private class BearbeitenButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JDialog dialog = new JDialog(VokabeltrainerGUI.this, "Vokabeln bearbeiten", true);
+            JPanel panel = new JPanel(new GridLayout(0, 2));
+            List<Vokabel> alleVokabeln = trainer.getAlleVokabeln();
+            for (Vokabel v : alleVokabeln) {
+                panel.add(new JLabel(v.getDeutsch() + ":"));
+                JTextField englischField = new JTextField(v.getEnglisch());
+                panel.add(englischField);
+                JButton speichernButton = new JButton("Speichern");
+                speichernButton.addActionListener(new SpeichernButtonHandler(v, englischField));
+                panel.add(speichernButton);
+                panel.add(new JLabel(""));
+            }
+            JScrollPane scrollPane = new JScrollPane(panel);
+            dialog.add(scrollPane);
+            dialog.pack();
+            dialog.setVisible(true);
+        }
+    }
+
+    private class SpeichernButtonHandler implements ActionListener {
+        private Vokabel vokabel;
+        private JTextField englischField;
+        public SpeichernButtonHandler(Vokabel vokabel, JTextField englischField) {
+            this.vokabel = vokabel;
+            this.englischField = englischField;
+        }
+        public void actionPerformed(ActionEvent e) {
+            String neuesEnglisch = englischField.getText();
+            vokabel.setEnglisch(neuesEnglisch);
+            trainer.speichereVokabeln();
+        }
+    }
+
+    public static void main(String[] args) {
+        Vokabeltrainer trainer = new Vokabeltrainer();
+        trainer.ladeVokabeln("vokabeln.txt");
+        new VokabeltrainerGUI(trainer);
+    }
+}
