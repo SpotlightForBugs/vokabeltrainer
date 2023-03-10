a2fa489a8235f035ca38bc7bb3e8a4e99c8715e6
Johannes Häusler <mail@spotlightforbugs.eu>
2023-03-10 10:28:22 +0100

    tried to implement Converter.java

---
+++ Converter.java
@@ -0,0 +1,29 @@
+import Java.util.List
+public class Converter{
+private Java.util.List<> temp;
+ 
+
+public java.util.List convert(List pList){
+
+pList(toFirst());
+
+while (!pList.isEmpty()){
+
+temp.add(pList.getContent());
+pList.remove()
+pList.next()
+
+
+
+
+}
+return temp;
+
+
+} 
+
+
+
+
+
+}
