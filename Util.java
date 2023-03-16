e8f5ea4d28c55e7543f49cfb142dcc9d7ba4994f
Spotlightforbugs <mail@spotlightforbugs.eu>
2023-03-16 12:28:45 +0100

    Added Docs

--- Util.java
+++ Util.java
@@ -5,6 +5,12 @@ public class Util {
 
 
 
+    /**
+     * Gibt die Größe der Liste zurück.
+     *
+     * @param list Die Liste, deren Größe ermittelt werden soll.
+     * @return Die Größe der Liste.
+     */
   public static int size(List<Vokabel> list) {
     int counter = 0;
     list.toFirst();
@@ -15,6 +21,12 @@ public class Util {
     return counter;
   }
 
+  /**
+   * Mischt die Vokabeln in der Liste.
+   *
+   * @param list Die Liste, die gemischt werden soll.
+   * @return Die gemischte Liste.
+   */
   public static List<Vokabel> shuffle(List<Vokabel> list) {
     Random random = new Random();
     int size = size(list);
@@ -35,6 +47,13 @@ public class Util {
     return list;
   }
 
+    /**
+     * Gibt zurück, ob die Liste den Inhalt enthält.
+     *
+     * @param list Die Liste, die durchsucht werden soll.
+     * @param content Der Inhalt, der gesucht wird.
+     * @return true, wenn die Liste den Inhalt enthält, sonst false.
+     */
   public boolean contains(List<ContentType> list, ContentType content) {
     list.toFirst();
     while (list.hasAccess()) {
