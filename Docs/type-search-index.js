6efcfa6fd39726eda577322edf7539894c14ba21
Spotlightforbugs <mail@spotlightforbugs.eu>
2023-03-16 12:20:34 +0100

    Added Docs

---
+++ Docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"List"},{"p":"<Unnamed>","l":"Util"},{"p":"<Unnamed>","l":"Vokabel"},{"p":"<Unnamed>","l":"Vokabeltrainer"},{"p":"<Unnamed>","l":"VokabeltrainerGUI"}];updateSearchResults();
